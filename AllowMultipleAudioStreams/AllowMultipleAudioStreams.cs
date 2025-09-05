9794d9b1ac970cde08874b36e822b7d8e281a6fb
ftfft <17285570+EIA485@users.noreply.github.com>
2025-09-05 16:53:40 -0700

    Ported to resonite

--- AllowMultipleAudioStreams/AllowMultipleAudioStreams.cs
+++ AllowMultipleAudioStreams/AllowMultipleAudioStreams.cs
@@ -1,56 +1,34 @@
-﻿using HarmonyLib;
-using NeosModLoader;
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.NET.Common;
+using BepInExResoniteShim;
+using FrooxEngine;
+using HarmonyLib;
 using System.Reflection;
-using System.Reflection.Emit;
-using System.Collections.Generic;
 
 namespace AllowMultipleAudioStreams
 {
-    public class AllowMultipleAudioStreams : NeosMod
+    [ResonitePlugin(PluginMetadata.GUID, PluginMetadata.NAME, PluginMetadata.VERSION, PluginMetadata.AUTHORS, PluginMetadata.REPOSITORY_URL)]
+    [BepInDependency(BepInExResoniteShim.PluginMetadata.GUID)]
+    public class AllowMultipleAudioStreams : BasePlugin
     {
-        public override string Name => "AllowMultipleAudioStreams";
-        public override string Author => "eia485";
-        public override string Version => "1.0.0";
-        public override string Link => "https://github.com/EIA485/NeosAllowMultipleAudioStreams/";
-        public override void OnEngineInit()
-        {
-            Harmony harmony = new Harmony("net.eia485.AllowMultipleAudioStreams");
-            harmony.PatchAll();
-        }
+        public override void Load() => HarmonyInstance.PatchAll();
 
         [HarmonyPatch]
         class AllowMultipleAudioStreamsPatch
         {
-            static MethodBase TargetMethod()
+            static MethodBase TargetMethod() //anonymous method that deletes AudioStreamSpawner instance
             {
-                return AccessTools.Method(AccessTools.TypeByName("FrooxEngine.AudioStreamSpawner+<>c__DisplayClass11_0"), "<OnStartStreaming>b__0");
-            }
-            static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
-            {
-                var codes = new List<CodeInstruction>(instructions);
-                for (int i = 0; i < codes.Count - 22; i++)
+                foreach(var type in typeof(FrooxEngine.AudioStreamSpawner).GetNestedTypes(BindingFlags.NonPublic))
                 {
-                    if (codes[i].opcode == OpCodes.Ldarg_0 &
-                        codes[i + 1].opcode == OpCodes.Ldfld &
-                        codes[i + 2].opcode == OpCodes.Ldsfld &
-                        codes[i + 3].opcode == OpCodes.Dup &
-                        codes[i + 4].opcode == OpCodes.Brtrue_S &
-                        codes[i + 5].opcode == OpCodes.Pop &
-                        codes[i + 6].opcode == OpCodes.Ldsfld &
-                        codes[i + 7].opcode == OpCodes.Ldftn &
-                        codes[i + 8].opcode == OpCodes.Newobj &
-                        codes[i + 9].opcode == OpCodes.Dup &
-                        codes[i + 10].opcode == OpCodes.Stsfld)
-                    {
-                        for (int si = i; si < i + 22; si++)
-                        {
-                            codes[si].opcode = OpCodes.Nop;
-                        }
-                        break;
-                    }
+                    var meth = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance)
+                        .FirstOrDefault(m => m.Name.Contains("OnStartStreaming") && m.ReturnType == typeof(void)
+                        && m.GetParameters().Length == 1 && m.GetParameters()[0].ParameterType == typeof(AudioStreamController));
+                    if (meth != null) return meth;
                 }
-                return codes;
+                return null;
             }
+            static bool Prefix() => false;
         }
     }
 }
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # AllowMultipleAudioStreams
 
-A [NeosModLoader](https://github.com/zkxs/NeosModLoader) mod for [Neos VR](https://neos.com/) that allows you to spawn multiple audio streams.
+A [BepisLoader](https://github.com/ResoniteModding/BepisLoader) mod for [Resonite](https://resonite.com/) that allows you to spawn multiple audio streams.
 
 ## Installation
-1. Install [NeosModLoader](https://github.com/zkxs/NeosModLoader).
-1. Place [AllowMultipleAudioStreams.dll](https://github.com/eia485/NeosAllowMultipleAudioStreams/releases/latest/download/AllowMultipleAudioStreams.dll) into your `nml_mods` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\NeosVR\nml_mods` for a default install. You can create it if it's missing, or if you launch the game once with NeosModLoader installed it will create the folder for you.
-1. Start the game. If you want to verify that the mod is working you can check your Neos logs.
+1. Install [BepisLoader](https://github.com/ResoniteModding/BepisLoader).
+1. Place [AllowMultipleAudioStreams.dll](https://github.com/eia485/NeosAllowMultipleAudioStreams/releases/latest/download/AllowMultipleAudioStreams.dll) into your `plugins` folder. This folder should be at `C:\Program Files (x86)\Steam\steamapps\common\Resonite\BepInEx\plugins` for a default install.
+1. Start the game. If you want to verify that the mod is working you can check your BepInEx log at `C:\Program Files (x86)\Steam\steamapps\common\Resonite\BepInEx\LogOutput.log`
